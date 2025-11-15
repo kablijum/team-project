62e55e51c6424d62a7d3a7d608bce136b613a978
AveryThompson7 <avery.thompson@mail.utoronto.ca>
2025-11-14 19:29:03 -0500

    PostPresenter

--- src/main/java/interface_adapter/post_review/PostPresenter.java
+++ src/main/java/interface_adapter/post_review/PostPresenter.java
@@ -1,4 +1,26 @@
 package interface_adapter.post_review;
 
-public class PostPresenter {
+import use_case.post_review.PostOutputData;
+import use_case.post_review.PostOutputDataBoundary;
+
+public class PostPresenter implements PostOutputDataBoundary{
+    private final PostViewModel postViewModel;
+
+    public  PostPresenter(PostViewModel postViewModel) {
+        this.postViewModel = postViewModel;
+    }
+    public void prepareSuccessView(PostOutputData outputData){
+        postViewModel.setComment(outputData.getComment());
+        postViewModel.setRating(outputData.getRating());
+        postViewModel.setUsername(outputData.getUsername());
+        postViewModel.setSongname(outputData.getSongname());
+
+    }
+    public void prepareFailureView(String errorMessage){
+        postViewModel.setErrorMessage(errorMessage);
+
+    }
+
+
+
 }
