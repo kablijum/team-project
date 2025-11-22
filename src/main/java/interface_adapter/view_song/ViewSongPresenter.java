37e8cf4901fa31dd163ca38092b10460f0ede9c2
AveryThompson7 <avery.thompson@mail.utoronto.ca>
2025-11-22 17:06:48 -0500

    changed avgRating to double and fixed typo

--- src/main/java/interface_adapter/view_song/ViewSongPresenter.java
+++ src/main/java/interface_adapter/view_song/ViewSongPresenter.java
@@ -16,7 +16,7 @@ public class ViewSongPresenter implements ViewSongOutputDataBoundary {
     @Override
     public void prepareSuccessView(ViewSongOutputData viewSongOutputData){
         final ViewSongState songState = viewSongViewModel.getState();
-        songState.setReviews(viewSongOutputData.getReviews);
+        songState.setReviews(viewSongOutputData.getReviews());
         songState.setAverageRating(viewSongOutputData.getAverageRating());
         songState.setSongName(viewSongOutputData.getSongName());
         songState.setArtist(viewSongOutputData.getArtist());
