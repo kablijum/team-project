75b7331b1ce31941854eec09623a88bd2bb27a94
Jumana Kabli <kablijumana@gmail.com>
2025-11-22 04:46:57 -0500

    Added data access interface for user and song

---
+++ src/main/java/use_case/edit_review/EditReviewSongDataAccessInterface.java
@@ -0,0 +1,16 @@
+package use_case.edit_review;
+
+import entity.Review;
+
+public interface EditReviewSongDataAccessInterface {
+
+    boolean existsByUsername(String username, int songId);
+
+    Review getReview(String username, int songId);
+
+    void updateReview(Review review, int songId);
+
+    int getAverageRating(int songId);
+
+    String getSongName(int songId);
+}
