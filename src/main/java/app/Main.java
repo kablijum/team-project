79550314c0249d1a85b1354e5c4cd7581849a689
Rylee <ryleechen17@gmail.com>
2025-11-24 15:32:54 -0500

    connect search result with SongProfileView, fixed the structure of data,firePropertyChanged method needed for ViewSongViewModel

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,7 +12,6 @@ public class Main {
                 .addSignupUseCase()
                 .addLoginUseCase()
                 .addSearchUseCase()
-                .addViewSongUseCase()
                 //.addChangePasswordUseCase()
                 .build();
 
