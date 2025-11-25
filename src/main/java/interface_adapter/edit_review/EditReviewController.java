372ca9b526791ed65da3c887ee8a45bcebc2ba56
Jumana Kabli <kablijumana@gmail.com>
2025-11-25 05:27:58 -0500

    Edit review controller

--- src/main/java/interface_adapter/edit_review/EditReviewController.java
+++ src/main/java/interface_adapter/edit_review/EditReviewController.java
@@ -1,4 +1,20 @@
 package interface_adapter.edit_review;
 
+import use_case.edit_review.EditInputData;
+import use_case.edit_review.EditInputDataBoundry;
+
 public class EditReviewController {
+
+    private final EditInputDataBoundry interactor;
+
+    public EditReviewController(EditInputDataBoundry interactor) {
+        this.interactor = interactor;
+    }
+
+    public void execute(String comment, int rating, String user, int songId) {
+
+        EditInputData inputData = new EditInputData(comment, rating, user, songId);
+        interactor.execute(inputData);
+
+    }
 }
