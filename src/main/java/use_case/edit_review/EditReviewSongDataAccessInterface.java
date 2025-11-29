727edaf2ce9e399a4149ba889fea45c704496cbc
Jumana Kabli <kablijumana@gmail.com>
2025-11-29 08:07:21 -0500

    debugged

--- src/main/java/use_case/edit_review/EditReviewSongDataAccessInterface.java
+++ src/main/java/use_case/edit_review/EditReviewSongDataAccessInterface.java
@@ -1,16 +1,8 @@
 package use_case.edit_review;
 
-import entity.Review;
 
 public interface EditReviewSongDataAccessInterface {
 
-    boolean existsByUsername(String username, int songId);
+    boolean existsByUsername(String username, int songid);
 
-    Review getReview(String username, int songId);
-
-    void updateReview(Review review, int songId);
-
-    double getAverageRating(int songId);
-
-    String getSongName(int songId);
 }
