8c661971f19f331f0e86691cab18f61b099b5b9a
iconniec <connieguo0607@gmail.com>
2025-11-29 20:22:19 -0500

    view_profile entity built

--- src/main/java/use_case/view_profile/ViewProfileReviewData.java
+++ src/main/java/use_case/view_profile/ViewProfileReviewData.java
@@ -1,5 +1,20 @@
 package use_case.view_profile;
 
 public class ViewProfileReviewData {
+    private final String songTitle;
+    private final int rating;
+    private final String comment;
+    private final int songId;
 
+    public ViewProfileReviewData(String songTitle, int rating, String comment, int songId) {
+        this.songTitle = songTitle;
+        this.rating = rating;
+        this.comment = comment;
+        this.songId = songId;
+    }
+
+    public String getSongTitle() { return songTitle; }
+    public int getRating() { return rating; }
+    public String getComment() { return comment; }
+    public int getSongId() { return songId; }
 }
