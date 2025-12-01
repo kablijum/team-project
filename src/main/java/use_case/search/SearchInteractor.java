e0169cf9a17d36005d65349f186edf0bb3dc1a50
Rylee <ryleechen17@gmail.com>
2025-11-30 22:14:11 -0500

    delete unused imports and fix some check style problems

--- src/main/java/use_case/search/SearchInteractor.java
+++ src/main/java/use_case/search/SearchInteractor.java
@@ -1,9 +1,21 @@
 package use_case.search;
 
 import entity.Song;
-
 import java.util.List;
 import java.util.stream.Collectors;
+
+/**
+ * The {@code SearchInteractor} implements the search use case in the application.
+ *
+ * <p>
+ * It coordinates the logic of searching for songs by delegating data retrieval to
+ * {@link SearchUserDataAccessInterface} and forwarding the result to
+ * {@link SearchOutputDataBoundary} for presentation.
+ *
+ * <p>
+ * This interactor does not perform any UI operations; it only contains application-level
+ * business rules, following the principles of Clean Architecture.
+ */
 
 public class SearchInteractor  implements SearchInputDataBoundary {
     private final SearchUserDataAccessInterface dataAccess;
@@ -34,5 +46,4 @@ public class SearchInteractor  implements SearchInputDataBoundary {
             presenter.prepareFailView("Search failed: " + e.getMessage());
         }
     }
-
 }
--- src/main/java/view/HomeView.java
+++ src/main/java/view/HomeView.java
@@ -4,15 +4,12 @@ import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.search.SearchController;
 import interface_adapter.search.SearchViewModel;
 import interface_adapter.view_song.ViewSongController;
-import interface_adapter.view_song.ViewSongState;
 import interface_adapter.view_song.ViewSongViewModel;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.view_profile_reviews.ProfileReviewsController;
 import use_case.search.SearchOutputData;
 
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -29,7 +26,7 @@ public class HomeView extends JPanel implements PropertyChangeListener {
     private final ViewSongViewModel viewSongViewModel;
     private final ViewManagerModel viewManagerModel;
     private ProfileReviewsController profileController;
-    private ViewSongController viewSongController;
+    private final ViewSongController viewSongController;
 
     public void setProfileController(ProfileReviewsController profileController) {
         this.profileController = profileController;
