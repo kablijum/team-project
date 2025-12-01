e08ab3d405e125aca1b0f41e313ee34c92fc17d7
multi-kontik <bayaraa.battsengel@mail.utoronto.ca>
2025-12-01 04:18:33 -0500

    Upvote checkstyle issues.

--- src/main/java/use_case/upvote/UpvoteSongDataAccessInterface.java
+++ src/main/java/use_case/upvote/UpvoteSongDataAccessInterface.java
@@ -1,14 +1,16 @@
 package use_case.upvote;
 
-import entity.Review;
-import entity.User;
-
 public interface UpvoteSongDataAccessInterface {
     /**
-     * Checks if the given username exists.
+     * Reflect the upvote action in the song database.
      * @param reviewUsername the username of the upvoted review.
      * @param songId the songid where upvoted review is written of.
      */
     void upvoteReview(String reviewUsername, int songId);
+    /**
+     * Reflect the downvote action in the song database.
+     * @param reviewUsername the username of the upvoted review.
+     * @param songId the songid where upvoted review is written of.
+     */
     void downvoteReview(String reviewUsername, int songId);
 }
