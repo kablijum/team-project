4a08097e3e55f2bc9ac9e8359ffc0dc4e91c5dde
AveryThompson7 <avery.thompson@mail.utoronto.ca>
2025-12-02 08:46:10 -0500

    fix

--- src/main/java/interface_adapter/view_song/ViewSongPresenter.java
+++ src/main/java/interface_adapter/view_song/ViewSongPresenter.java
@@ -21,6 +21,7 @@ public class ViewSongPresenter implements ViewSongOutputDataBoundary {
         songState.setSongName(viewSongOutputData.getSongName());
         songState.setArtist(viewSongOutputData.getArtist());
         songState.setSongId(viewSongOutputData.getSongId());
+        songState.setMessage(null);
 
         // Notify ViewModel listener
         this.viewSongViewModel.firePropertyChange();
@@ -38,6 +39,7 @@ public class ViewSongPresenter implements ViewSongOutputDataBoundary {
         songState.setArtist(viewSongOutputData.getArtist());
         songState.setSongId(viewSongOutputData.getSongId());
         songState.setReviews(new java.util.HashMap<>());
+
 
         this.viewSongViewModel.firePropertyChange();
 
